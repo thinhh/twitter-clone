762bb2a8eabf0656528579fc4700e650f65d3128
thinhh <thinh_trang942000@yahoo.com>
2024-08-27 18:55:38 -0700

    auth and protected routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient , QueryClientProvider } from '@tanstack/react-query'
+const queryClient  = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, 
+    },
+  },
+});
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+      </BrowserRouter>
    
   </React.StrictMode>,
 )
